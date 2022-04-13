37a47482dae373a4fcdbfae631fe530d1798b35a
Alvaro-M-R <45154602+Alvaro-M-R@users.noreply.github.com>
2022-04-13 04:24:17 -0400

    Add files via upload

---
+++ pregunta5/preg5.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 13 03:05:58 2022
+
+@author: Alvaro
+"""
+
+from sklearn import tree
+from sklearn import datasets as ds
+
+iris = ds.load_iris()
+print(iris)
+
+x = iris.data
+y = iris.target
+
+print(x)
+print(y)
+
+clasificador = tree.DecisionTreeClassifier()
+print(clasificador)
+resultado=clasificador.fit(x,y)
+xp=[[5.3, 3.7, 1.5, 0.2],
+    [5. , 3.5, 1.3, 0.3],
+    [4.5, 2.3, 1.3, 0.3],
+    [5.,  3.3, 1.4, 0.2],
+    [7.,  3.2, 4.7, 1.4],
+    [6.4, 3.2, 4.5, 1.5]]
+yp=clasificador.predict(xp)
+print(yp)
+
+
+
+
